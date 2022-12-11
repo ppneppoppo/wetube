61f72abc242874d0fae1fdf949193274999ce082
ppneppoppo <110282100+ppneppoppo@users.noreply.github.com>
2022-12-11 20:56:56 +0900

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,22 @@
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const path = require("path");
+
+const BASE_JS = "./src/client/js/";
+
 module.exports = {
-  entry: "./src/client/js/main.js",
-  mode: "development",
-  watch: true,
+  entry: {
+    main: BASE_JS + "main.js",
+    videoPlayer: BASE_JS + "videoPlayer.js",
+    recorder: BASE_JS + "recorder.js",
+    commentSection: BASE_JS + "commentSection.js",
+  },
   plugins: [
     new MiniCssExtractPlugin({
       filename: "css/styles.css",
     }),
   ],
   output: {
-    filename: "js/main.js",
+    filename: "js/[name].js",
     path: path.resolve(__dirname, "assets"),
     clean: true,
   },
