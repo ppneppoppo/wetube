12b5178ff4710d94de7a9cef43622d9f30c742f2
ppneppoppo <110282100+ppneppoppo@users.noreply.github.com>
2022-12-11 21:04:15 +0900

    Update videoRouter.js

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -8,7 +8,9 @@ import {
   deleteVideo,
 } from "../controllers/videoController";
 import { protectorMiddleware, videoUpload } from "../middlewares";
+
 const videoRouter = express.Router();
+
 videoRouter.get("/:id([0-9a-f]{24})", watch);
 videoRouter
   .route("/:id([0-9a-f]{24})/edit")
@@ -23,7 +25,6 @@ videoRouter
   .route("/upload")
   .all(protectorMiddleware)
   .get(getUpload)
-  .post(videoUpload.single("video"), postUpload);
   .post(videoUpload.fields([{ name: "video" }, { name: "thumb" }]), postUpload);
 
 export default videoRouter;
