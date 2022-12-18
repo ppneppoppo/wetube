50bb313c4e9a7b8b00e7b35bcb620514b41f64d6
ppneppoppo <110282100+ppneppoppo@users.noreply.github.com>
2022-12-18 18:03:02 +0900

    Update middlewares.js

--- src/middlewares.js
+++ src/middlewares.js
@@ -13,13 +13,13 @@ const isHeroku = process.env.NODE_ENV === "production";
 
 const s3ImageUploader = multerS3({
   s3: s3,
-  bucket: "wetubeee/images",
+  bucket: "wetube/images",
   acl: "public-read",
 });
 
 const s3VideoUploader = multerS3({
   s3: s3,
-  bucket: "wetubeee/videos",
+  bucket: "wetube/videos",
   acl: "public-read",
 });
 
