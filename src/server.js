c8f6447339e42808a3610aa96e247cb43245b2ce
ppneppoppo <110282100+ppneppoppo@users.noreply.github.com>
2022-12-18 19:19:06 +0900

    Update server.js

--- src/server.js
+++ src/server.js
@@ -8,30 +8,28 @@ import videoRouter from "./routers/videoRouter";
 import userRouter from "./routers/userRouter";
 import apiRouter from "./routers/apiRouter";
 import { localsMiddleware } from "./middlewares";
-
 const app = express();
 const logger = morgan("dev");
-
 app.set("view engine", "pug");
 app.set("views", process.cwd() + "/src/views");
 app.use(logger);
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(
-  session({
-    secret: process.env.COOKIE_SECRET,
-    resave: false,
-    saveUninitialized: false,
-    store: MongoStore.create({ mongoUrl: process.env.DB_URL }),
-  })
+    session({
+        secret:process.env.COOKIE_SECRET,
+        resave:false,
+        saveUninitialized:false,
+        store:MongoStore.create({ mongoUrl:process.env.DB_URL })
+    })
 );
 app.use(flash());
 app.use(localsMiddleware);
-app.use("/uploads", express.static("uploads"));
+app.use("/upload", express.static("upload"));
 app.use("/static", express.static("assets"));
 app.use("/", rootRouter);
 app.use("/videos", videoRouter);
 app.use("/users", userRouter);
 app.use("/api", apiRouter);
 
-export default app;
+export default app
